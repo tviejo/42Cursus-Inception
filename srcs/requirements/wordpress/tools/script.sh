22e617a57de286ca1c81c89da6cbee3ee73c408b
tviejo <tviejo12@gmail.com>
2024-11-02 14:06:31 +0100

    WOOORRRRRKKKIIIINNGGG

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -8,7 +8,7 @@ else
 
 	wp core config --dbname=${SQL_DATABASE} --dbuser=${SQL_USER} --dbpass=$(cat /run/secrets/db_password) --dbhost="mariadb" --allow-root
 
-	wp core install --url=${WP_URL} --title=${SQL_DATABASE} --admin_user=${WP_ROOT} --admin_password=$(cat /run/secrets/db_root_password) --admin_email=${WP_ROOT_EMAIL} --allow-root
+	wp core install --url=${WP_URL} --title=${WP_TITLE} --admin_user=${WP_ADMIN_USER} --admin_password=${WP_ADMIN_PASSWORD} --admin_email=${WP_ADMIN_EMAIL} --allow-root
 
 	wp user create ${WP_USER} ${WP_USER_EMAIL} --role=author --user_pass=$(cat /run/secrets/wp_password) --allow-root
 
